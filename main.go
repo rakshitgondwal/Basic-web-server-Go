e3b33956dfc7f67864430a694b2270c0ef7f8ec8
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-29 04:30:27 +0530

    added helloHandler func

--- main.go
+++ main.go
@@ -6,10 +6,33 @@ import (
 	"net/http"
 )
 
-func main() {
-	response, error := http.Get("https://localhost:3000")
-	if error != nil {
-		log.Print(error)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
 	}
-	fmt.Print(response)
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, "hello!")
+}
+
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/post", formHandler)
+
+	fmt.Printf("Server started on port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
